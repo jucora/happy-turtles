fa62e354534aa66f715f34aaf5dfcda23f0e2e2c
Julian Belmonte <32948886+jucora@users.noreply.github.com>
2019-08-20 09:56:13 -0500

    Julians code

--- 1.1 Data structure.rb
+++ 1.1 Data structure.rb
@@ -23,3 +23,30 @@ list = LinkedList.new
 list.add(3)
 list.add(5)
 puts list.get(1)
+
+
+---------JULIAN'S CODE-----------
+
+class LinkedList
+  #setup head and tail
+  
+  def initialize
+    @list=[]
+  end
+   
+  def add(number)
+    # your code here
+    @list << number
+  end
+
+  def get(index)
+    # your code here
+    return @list[index]
+  end
+end
+
+list = LinkedList.new
+
+list.add(3)
+list.add(5)
+puts list.get(1)
